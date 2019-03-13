b167cebf3bb8824b437dccafe313ce376ce13ff3
ペリーも恐れるペリカン <kazuyaman1413@gmail.com>
2019-03-13 18:05:23 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum:15 }
+  validates :email, presence:true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  has_secure_password
+end
